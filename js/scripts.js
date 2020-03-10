c3cad1fcd812b5cfca7533b52ff653089dd4fc30
EJ Serafin <edgardo@chainblx.com>
2020-03-10 00:41:04 -0700

    created global variables

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// global variables
+const randomUrl = 'https://randomuser.me/api/';
+const searchContainer = document.querySelector('.search-container');
+const gallery = document.getElementById('gallery');
+
