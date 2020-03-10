60c7e5f3b9c066c834b3c72039cf335087607891
EJ Serafin <edgardo@chainblx.com>
2020-03-10 01:00:36 -0700

    created fetch api
    created fetch api and modified the url of the random user API to get 12 users
    
    currently logging data to the console

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 // global variables
-const randomUrl = 'https://randomuser.me/api/';
+const randomUrl = 'https://randomuser.me/api/?results=12';
 const searchContainer = document.querySelector('.search-container');
 const gallery = document.getElementById('gallery');
 
+// fetch
+fetch(randomUrl)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
