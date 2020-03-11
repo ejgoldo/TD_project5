fc126191dc7bd28d211cc8237777c0cf9d9563ef
EJ Serafin <edgardo@chainblx.com>
2020-03-10 22:55:58 -0700

    remade fetch api and generated users
    split the fetch api
    12 people from the random user api are now generated on the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,49 @@
 // global variables
-const randomUrl = 'https://randomuser.me/api/?nat=us&inc=picture,name,location,email&results=12';
+const randomUrl = 'https://randomuser.me/api/?nat=us&results=12';
 const searchContainer = document.querySelector('.search-container');
 const gallery = document.getElementById('gallery');
 
+// fetch function
+/*
+this fetch function separates code that would have cluttered the fetch api.
+this is more readable and separates the respond to the actual implementation of the returned data from the API
+*/
+function fetchData(url){
+    return fetch(url)
+        .then(checkStatus)
+        .then(response => response.json())
+        .catch(error => console.log('An error occurred', error));
+}
+
 // fetch
-fetch(randomUrl)
-    .then(response => {
-        return response.json();
-    })
+fetchData(randomUrl)
     .then(data => {
-        console.log(data);
+        generateGallery(data.results)
     })
+
+// functions
+function checkStatus(response){
+    if(response.ok){
+        return Promise.resolve(response)
+    } else {
+        return Promise.reject(new Error(response.statusText))
+    }
+}
+
+function generateGallery(users){
+    const html = users.map(data => {
+        return `
+        <div class="card">
+            <div class="card-img-container">
+                <img class="card-img" src="${data.picture.medium}" alt="profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">${data.name.first} ${data.name.last}</h3>
+                <p class="card-text">${data.email}</p>
+                <p class="card-text cap">${data.location.city}, ${data.location.state}</p>
+            </div>
+        </div>
+        `
+        });
+    gallery.innerHTML = html.join('');
+}
